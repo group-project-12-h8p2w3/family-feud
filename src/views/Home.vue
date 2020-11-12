647f9b14ded80edb0a730146ebb009a563207606
yosiaelnino <yosia.elnino@gmail.com>
2020-11-12 23:55:41 +0700

    1. Create base template for homepage 2. Create base template for waiting room page 3. Create main logo for the apps 4. Create favicon

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container d-flex justify-content-center main-home">
+      <div class="align-self-center">
+        <div class="card welcome-card">
+          <div class="card-body d-flex flex-column justify-content-center">
+            <div class="align-self-center">
+              <img class="logo" src="../assets/logo-hacktiv-feud.svg" alt="logo">
+              <h1 class="welcome-title">Welcome to HacktivFeud</h1>
+              <form @submit.prevent="login">
+                <input v-model="username" type="text" placeholder="enter username">
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    login () {
+      this.$router.push('/rooms')
+    }
   }
 }
 </script>
+
+<style>
+  .main-home {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .welcome-card {
+    box-shadow: 0px 3px 15px rgba(0,0,0,0.4);
+    border-radius: 25px !important;
+    width: 50vw;
+    height: 55vh;
+  }
+  .welcome-card-body {
+    margin: auto;
+  }
+  .welcome-title {
+    color: #3c5da3;
+    margin-bottom: 32px;
+  }
+  .logo {
+    width: 20vw;
+  }
+  form input[type=text] {
+    border: 0;
+    border-radius: 5px;
+    height: 5vh;
+    width: 30vw;
+    box-shadow: 5px 5px 15px rgba(0,0,0,0.3)
+  }
+  ::placeholder {
+    text-align: center;
+    font-size: 25px;
+    color: #3c5da3;
+  }
+  input {
+    text-align: center;
+    font-size: 25px;
+    color: #3c5da3;
+  }
+</style>
--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h1>This is a rooms page</h1>
+    <div class="row">
+      <div class="col d-flex flex-column justify-content-center illustration-side">
+        <div class="align-self-center">
+          <img class="waiting-illustration" src="../assets/waiting-illustration.svg" alt="">
+          <h1 class="text-muted waiting-room-title">Waiting Room...</h1>
+        </div>
+      </div>
+      <div class="col-7 d-flex flex-column justify-content-center">
+        <div class="align-self-center">
+          <div class="card rooms-card">
+            <div class="card-body">
+              <h1>Rooms</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +27,23 @@ export default {
 </script>
 
 <style>
+  .waiting-illustration {
+    width: 30vw;
+  }
 
+  .illustration-side {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .waiting-room-title {
+    margin-top: 16px;
+  }
+
+  .rooms-card {
+    width: 45vw;
+    height: 50vh;
+    box-shadow: 0px 3px 15px rgba(0,0,0,0.4);
+    border-radius: 25px !important;
+  }
 </style>
